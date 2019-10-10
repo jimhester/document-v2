587fcf9a5bf12b6cf0e1e6678877e6e991f796ba
Jim Hester <james.f.hester@gmail.com>
2019-10-10 09:14:10 -0400

    We don't need to create the remote here anymore

--- src/main.ts
+++ src/main.ts
@@ -29,8 +29,6 @@ async function run() {
       `https://x-access-token:${token}@`
     );
 
-    await exec.exec("git", ["remote", "add", "pr", headCloneURL2]);
-
     await exec.exec("git", ["push", "pr", `HEAD:${headBranch}`]);
   } catch (error) {
     core.setFailed(error.message);
